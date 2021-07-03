6f73f322b66948525d6a7babe0e2f5f52a222c04
higg <higginshiggins2@protonmail.com>
2021-07-03 10:38:20 -0700

    cleaned up code and improved comments

--- contact-mic-comparator-trigger.ino
+++ contact-mic-comparator-trigger.ino
@@ -4,18 +4,17 @@ int ledPin = 13;      // select the pin for the LED
 double mic_value = 0;  // variable to store the value coming from the sensor
 
 void setup() {
-  // declare the ledPin as an OUTPUT:
+  // declare the ledPin as an OUTPUT
   pinMode(ledPin, OUTPUT);
 }
 
 void loop() {
   // read the value from the sensor:
   mic_value = analogRead(contact_mic);
-  // turn the ledPin on
+  // turn the ledPin on if piezo transducer signal is greater than 0.83
   if (sin(mic_value) > 0.83) {
     digitalWrite(13, HIGH);
-  //int print(contact_mic);
-    delay(2*1000);
+    delay(2*1000);  // maintain logical HIGH for 2 seconds to prevent system oscillation
   }
   else {
     digitalWrite(13, LOW);
